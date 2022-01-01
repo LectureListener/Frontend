833e5fa1f486d55a757df60beea4326dca2362d0
loukylor <Loukylor@users.noreply.github.com>
2022-01-01 13:45:18 -0800

    add an onFinish

--- src/sdk/structures/Job.js
+++ src/sdk/structures/Job.js
@@ -8,6 +8,17 @@ class Job {
     async fetchStatus() {
         return await this.client.api.endpoint(this.endpointUrl, "GET")
     }
+
+    onFinish(callback) {
+        const wait = async () => {
+            const { status } = await this.fetchStatus()
+            if (status == "completed")
+                callback()
+            else    
+                setTimeout(wait, 1000);
+        }
+        setTimeout(wait, 1000);
+    }
 }
 
 module.exports = Job
