2df869998812997ecc6c73abf6a34d8d0944f705
WenqiLiao <95704237+WenqiLiao@users.noreply.github.com>
2022-01-01 21:05:59 -0500

    Update AppHeader.jsx
    add three bottoms to the header. But have not function them yet

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -10,6 +10,15 @@ const AppHeader = () => {
         <div id="header" className="header d-flex vw-100">
             <button onClick={() => displayInputPage(true)} class="btn menu-button bg-secondary text-white">Menu</button>
             {inputPage ? <InputPage return={() => displayInputPage(false)}/> : null }
+            <div className="newButton">
+                <botton type="button" onclick="'InputPage.jsx'">new</botton>
+            </div>
+            <div className="saveButton">
+                <botton type="button">save</botton>
+            </div>
+            <div className="loadButton">
+                <botton type="button">load</botton>
+            </div>
             <div className="title d-flex mx-auto">
                 <img className="image logo" alt="logo" src={logo}></img>
                 <h4>Lecture Listener</h4>
