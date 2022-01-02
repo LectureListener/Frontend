c7ed115ff276d625f2f2d82fd2573fab6af87a94
Christine Zhao <christine.zhao@mail.utoronto.ca>
2022-01-01 21:47:03 -0500

    added toggles

--- src/components/templates/Toolbar.jsx
+++ src/components/templates/Toolbar.jsx
@@ -2,6 +2,7 @@ const Toolbar = (props) => {
     return (
         <div className="toolbar p-4 my-0 d-flex justify-content-between">
             <button class="btn text-white">Highlight Section</button>
+            <button class="btn text-white" onClick={props.toggleTranscript}>{props.isFullTranscript ? "View Highlighted Sections" : "View Full Transcript"}</button>
             <button class="btn text-white">Add Comment</button>
         </div>
     )
