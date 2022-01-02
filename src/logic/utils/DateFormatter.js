d53bf940dc591adad58a0bbacd731629cf385c41
loukylor <Loukylor@users.noreply.github.com>
2022-01-01 16:23:23 -0800

    remove padding on hours and minutes

--- src/logic/utils/DateFormatter.js
+++ src/logic/utils/DateFormatter.js
@@ -2,9 +2,9 @@ class DateFormatter {
     static toMinutesAndHours(date) {
         // If it lasts into the hours
         if (date.getUTCHours())
-            return `${date.getUTCHours().toString().padStart(2, "0")}:${date.getUTCMinutes().toString().padStart(2, "0")}:${date.getUTCSeconds().toString().padStart(2, "0")}`
+            return `${date.getUTCHours().toString()}:${date.getUTCMinutes()}:${date.getUTCSeconds().toString().padStart(2, "0")}`
         else
-            return `${date.getUTCMinutes().toString().padStart(2, "0")}:${date.getUTCSeconds().toString().padStart(2, "0")}`
+            return `${date.getUTCMinutes()}:${date.getUTCSeconds().toString().padStart(2, "0")}`
     }
 }
 
